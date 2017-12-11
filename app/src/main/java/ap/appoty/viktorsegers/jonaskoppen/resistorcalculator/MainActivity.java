94660797d9173082053790e9d4d795963355e1be
Joren <jorenjamar@hotmail.com>
2017-12-11 15:33:38 +0100

    basis listview startscherm

--- app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/MainActivity.java
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/MainActivity.java
@@ -2,6 +2,12 @@ package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +16,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView listView = (ListView)findViewById(R.id.listView);
 
-        int text = 0;
+
+
+
+    }
+
+    class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.main_listview, null);
+
+            TextView textView_textList =(TextView)view.findViewById(R.id.textView);
+            //ImageView imageView_photoList = (ImageView)view.findViewById(R.id.imageView);
+
+            textView_textList.setText("hey");
+
+            //imageView_photoList.setImageResource(appointmentList.get(i).getPicture());
+
+            return view;
+        }
+
     }
 }
+
