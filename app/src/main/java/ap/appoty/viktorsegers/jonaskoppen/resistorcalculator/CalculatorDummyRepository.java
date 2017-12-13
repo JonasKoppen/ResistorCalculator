3df4a9a5f289a50a53bdcc9bced1a2979104bf0e
Viktor Segers <segersviktor@hotmail.com>
2017-12-13 09:53:16 +0100

    Making dummy repository

--- app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/CalculatorDummyRepository.java
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/CalculatorDummyRepository.java
@@ -1,8 +1,26 @@
 package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
 
+import java.util.List;
+
 /**
  * Created by Viktor on 13-12-2017.
  */
 
-public class CalculatorDummyRepository {
+public class CalculatorDummyRepository implements ICalculatorRepository{
+
+    private static ICalculatorRepository repo = null;
+
+    public static ICalculatorRepository getInstance() {
+        if (repo == null) {
+            repo = new CalculatorDummyRepository() {
+
+            };
+        }
+        return repo;
+    }
+
+    public List<Calculater> getCalculater(){
+
+    }
+
 }
---
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/ICalculatorRepository.java
@@ -0,0 +1,11 @@
+package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
+
+import java.util.List;
+
+/**
+ * Created by Viktor on 13-12-2017.
+ */
+
+public interface ICalculatorRepository {
+    List<Calculater> getCalculator();
+}
