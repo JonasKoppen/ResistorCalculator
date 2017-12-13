bb85d0997a17ad33e0873b1c3161e8cc20e76453
Jonas Koppen DRWork <dreaddrone@gmail.com>
2017-12-13 10:34:48 +0100

    Added colors for the selector for the resistor calculator

---
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/ResistorCalcBtnValue.java
@@ -0,0 +1,17 @@
+package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
+
+/**
+ * Created by jonas on 13/12/2017.
+ */
+
+public class ResistorCalcBtnValue {
+    private int value;
+    private int colorCode;
+    private int id;
+
+    public ResistorCalcBtnValue(int id, int value, int colorCode){
+        this.id  = id;
+        this.colorCode = colorCode;
+        this.value = value;
+    }
+}
