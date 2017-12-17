16947bffd0d05c991210e54340d27d718c36af1b
Joren <jorenjamar@hotmail.com>
2017-12-17 21:25:35 +0100

    Parallel werkt volledig, grote van tekst voor uitkomst bij parallel en serie is aangepast en prefix bij uitkomst werkt bij serie en parallel

--- app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/resistor_serie.java
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/resistor_serie.java
@@ -14,6 +14,7 @@ public class resistor_serie extends AppCompatActivity {
     TextView totalText;
     EditText R1Text;
     EditText R2Text;
+    String prefix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +44,42 @@ public class resistor_serie extends AppCompatActivity {
     public void bereken(View v){
         String R1string = R1Text.getText().toString();
         String R2string = R2Text.getText().toString();
-        int R1 = Integer.parseInt(R1string);
-        int R2 = Integer.parseInt(R2string);
-        int Rtotal = R1 + R2;
+        double R1 = Double.parseDouble(R1string);
+        double R2 = Double.parseDouble(R2string);
+        double Rtotal = prefixCalculator(R1 + R1);
         String RtotalString = String.valueOf(Rtotal);
-        totalText.setText(RtotalString + "\tΩ");
+
+        totalText.setText(RtotalString + prefix);
+    }
+
+    private Double prefixCalculator(Double getal){
+        if(getal % Math.pow(10, 8) == 0){
+            prefix = "GΩ";
+            return (getal/Math.pow(10,9));
+        }
+        else if(getal % Math.pow(10, 5) == 0){
+            prefix = "MΩ";
+            return (getal/Math.pow(10,6));
+        }
+        else if(getal % Math.pow(10, 2) == 0){
+            prefix = "KΩ";
+            return (getal/Math.pow(10,3));
+        }
+        else if(getal % 1 == 0){
+            prefix = "Ω";
+            return getal;
+        }
+        else if(getal % Math.pow(10, -4) == 0){
+            prefix = "mΩ";
+            return (getal/Math.pow(10,-3));
+        }
+        else if(getal % Math.pow(10, -7) == 0){
+            prefix = "µΩ";
+            return (getal/Math.pow(10,-6));
+        }
+        else{
+            prefix = "nΩ";
+            return (getal/Math.pow(10,-9));
+        }
     }
 }
