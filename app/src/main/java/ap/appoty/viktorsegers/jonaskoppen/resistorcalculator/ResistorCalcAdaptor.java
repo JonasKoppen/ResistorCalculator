bd689141778caddbd403b3a4964ca0134de3997b
JonasKoppen_DROS <dreaddrone@gmail.com>
2017-12-17 22:19:17 +0100

    Fixed color problem of the resistor calculatorg

--- app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/ResistorCalcAdaptor.java
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/ResistorCalcAdaptor.java
@@ -1,11 +1,14 @@
 package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,7 +26,8 @@ public class ResistorCalcAdaptor extends ArrayAdapter<ResistorCalcBtnValue> {
                         ViewGroup parent){
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = inflater.inflate(R.layout.resistor_calc_list, null, false);
-        itemView.findViewById(R.id.btnCalcItem).setBackgroundColor(getItem(position).getColorCode());
+        ((TextView)itemView.findViewById(R.id.btnCalcItem)).setBackgroundColor(ContextCompat.getColor(getContext(), getItem(position).getColorCode()));
+        ((TextView)itemView.findViewById(R.id.btnCalcItem)).setText( String.valueOf(getItem(position).getValue()));
         return itemView;
     }
 }
