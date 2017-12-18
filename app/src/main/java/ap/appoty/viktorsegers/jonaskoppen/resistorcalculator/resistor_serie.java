b4f217679b43958eb64abb62ab4739fa50305522
Joren <jorenjamar@hotmail.com>
2017-12-18 16:26:29 +0100

    serie werkt met prefix voor ingegeven waarden

--- app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/resistor_serie.java
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/resistor_serie.java
@@ -3,9 +3,13 @@ package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -14,6 +18,9 @@ public class resistor_serie extends AppCompatActivity {
     TextView totalText;
     EditText R1Text;
     EditText R2Text;
+    Spinner R1Spinner;
+    Spinner R2Spinner;
+
     String prefix;
 
     @Override
@@ -24,6 +31,14 @@ public class resistor_serie extends AppCompatActivity {
         totalText = (TextView)findViewById(R.id.textView);
         R1Text = (EditText)findViewById(R.id.editTextR1);
         R2Text = (EditText)findViewById(R.id.editTextR2);
+
+        R1Spinner = (Spinner)findViewById(R.id.planets_spinnerR1);
+        R2Spinner = (Spinner) findViewById(R.id.planets_spinnerR2);
+
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(this, R.array.prefixOhm, android.R.layout.simple_spinner_item);
+
+        R1Spinner.setAdapter(arrayAdapter);
+        R2Spinner.setAdapter(arrayAdapter);
 
 
 
@@ -42,10 +57,42 @@ public class resistor_serie extends AppCompatActivity {
         */
     }
     public void bereken(View v){
+        double R1multiplier = 1;
+        switch (R1Spinner.getSelectedItemPosition()){
+            case 0:
+                R1multiplier = 1;
+                break;
+            case 1 :
+                R1multiplier = Math.pow(10,3);
+                break;
+            case 2 :
+                R1multiplier = Math.pow(10,6);
+                break;
+            case 3 :
+                R1multiplier = Math.pow(10,9);
+                break;
+        }
+
+        double R2multiplier = 1;
+        switch (R2Spinner.getSelectedItemPosition()){
+            case 0:
+                R2multiplier = 1;
+                break;
+            case 1 :
+                R2multiplier = Math.pow(10,3);
+                break;
+            case 2 :
+                R2multiplier = Math.pow(10,6);
+                break;
+            case 3 :
+                R2multiplier = Math.pow(10,9);
+                break;
+        }
+
         String R1string = R1Text.getText().toString();
         String R2string = R2Text.getText().toString();
-        double R1 = Double.parseDouble(R1string);
-        double R2 = Double.parseDouble(R2string);
+        double R1 = Double.parseDouble(R1string) * R1multiplier;
+        double R2 = Double.parseDouble(R2string) * R2multiplier;
         double Rtotal = prefixCalculator((R1 + R2));
         String RtotalString = String.valueOf(Rtotal);
 
