04db34c82783c9788e6c56a841f570c8973fb296
JonasKoppen_DROS <dreaddrone@gmail.com>
2017-12-19 22:23:27 +0100

    start of color calculator, stay out of the color calculato, it is broken

---
+++ app/src/main/java/ap/appoty/viktorsegers/jonaskoppen/resistorcalculator/ResistorCalcView.java
@@ -0,0 +1,26 @@
+package ap.appoty.viktorsegers.jonaskoppen.resistorcalculator;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**
+ * Created by jonas on 19/12/2017.
+ */
+
+public class ResistorCalcView extends SurfaceView {
+    public ResistorCalcView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        canvas.drawCircle(10,10,5, paint);
+    }
+
+
+}
